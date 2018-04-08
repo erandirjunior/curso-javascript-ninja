121312b24f8463f46132f9391497e9f608075e33
Erandir Junior <aefs12junior@gmail.com>
2018-04-08 11:02:37 -0300

    Operadores unários

---
+++ challenge-02/aula02.js
@@ -0,0 +1,29 @@
+// Operadores Unários -> + e - //undefined
+
+3 + 3; //6
+
++3; //3
+
++'3'; //3
+
+// NaN not a number -> retornando quando uma operação matemática falha ou quando uma função tenta transformar uma string em inteiro
+
++'junior'; //NaN
+
+// Concatenação
+
+'jun' + 'ior'; //'junior'
+
+'3' + 3; //'33'
+
+3 + '3'; //'33'
+
+var x = 3; //undefined
+
+x; //3
+
+-x; //-3
+
+x; //3
+
+-'3'; //-3
