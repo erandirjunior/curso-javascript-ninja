8b3619f9071ec739f3b1094516dc2078b4d59be7
Erandir Junior <aefs12junior@gmail.com>
2018-04-08 11:41:57 -0300

    Métodos de objeto (continuação)

---
+++ challenge-03/aula03.js
@@ -0,0 +1,13 @@
+// Métodos Continuação
+
+// Funções dentro de objeto -> chamam-se métodos
+
+// Variáveis dentro de objeto -> chamam-se propriedades
+
+var pessoa = {nome: 'Junior', idade: 22, peso: 53, altura: 1.61, sexo: 'masculino'}; //undefined
+
+pessoa.andar = function() {
+	return 'pessoa andando';
+	} //[Function]
+
+pessoa.andar(); //'pessoa andando'
