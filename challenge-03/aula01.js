eb9a7de0d13bc3d411434cca0add62da9b97cf29
Erandir Junior <aefs12junior@gmail.com>
2018-04-08 11:39:12 -0300

    Tipos de dados e objetos

---
+++ challenge-03/aula01.js
@@ -0,0 +1,43 @@
+// Tipos de Dados
+
+// Tipos Primitivos -> number, string, boolean, null e undefined
+
+// Tipos de Objeto -> todos os outros tipos de dados
+
+// Objeto -> conjunto de propriedades
+
+{ propriedade: 'valor' } //{ propriedade: 'valor' }
+
+var objeto = {propriedade: 'valor', propriedade2: 10, propriedade3: true}; //undefined
+
+objeto; //{ propriedade: 'valor', propriedade2: 10, propriedade3: true }
+
+objeto.propriedade; //'valor'
+
+var pessoa = {nome: 'Junior', idade: 22, peso: 53, altura: 1.61}; //undefined
+
+pessoa; //{ nome: 'Junior', idade: 22, peso: 53, altura: 1.61 }
+
+pessoa.nome; //'Junior'
+
+pessoa.idade; //22
+
+pessoa.altura; //1.61
+
+var carro = {cor: 'vermelho', motor: '2.0', portas: 4}; //undefined
+
+carro; //{ cor: 'vermelho', motor: '2.0', portas: 4 }
+
+carro.cor; //'vermelho'
+
+carro.motor; //'2.0'
+
+carro.portas; //4
+
+pessoa; //{ nome: 'Junior', idade: 22, peso: 53, altura: 1.61 }
+
+var myvar = function() { return 'variável myvar'; }; //undefined
+
+myvar; //[Function: myvar]
+
+myvar(); //'variável myvar'
