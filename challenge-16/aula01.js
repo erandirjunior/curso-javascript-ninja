5ff460847aa61990902a2a837b9faf1941fb524f
Erandir Junior <aefs12junior@gmail.com>
2018-04-22 17:02:32 -0300

    use strict - criação de variáveis sem o var

---
+++ challenge-16/aula01.js
@@ -0,0 +1,11 @@
+(function() {
+
+	// use strict -> uma diretiva que faz o browser interpretar o código javascript no modo mais rigoroso
+
+	'use strict';
+
+	// Não permite a criação de variáveis sem o uso do var
+
+	myName = 'Erandir Junior'; //Uncaught ReferenceError: myName is not defined
+	
+})();
