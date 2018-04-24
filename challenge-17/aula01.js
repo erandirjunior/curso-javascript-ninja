61134c6776a60d5a2e8c6d0095ed44ecdcb2200d
Erandir Junior <aefs12junior@gmail.com>
2018-04-24 20:33:47 -0300

    Regex - parte 1

---
+++ challenge-17/aula01.js
@@ -0,0 +1,16 @@
+// Expressões regulares -> manipulam strings, é um tipo primitivo
+
+var regex = /m/; //undefined
+regex; ///m/
+
+// RegExp() -> objeto de expressão regular
+
+// match() -> tentar fazer o match da string com a expressão regular
+
+var text = 'Manuel Marques de Sousa, Conde de Porto Alegre (Rio Grande, 13 de junho de 1804 – Rio de Janeiro, 18 de julho de 1875), apelidado de "O Centauro de Luvas",[nota 1] foi um militar, político, abolicionista e monarquista brasileiro. Ele nasceu em uma família rica e de tradição militar, entrando no exército em 1817 quando ainda era criança. Sua iniciação militar ocorreu na Guerra contra Artigas, que teve seu território anexado e se tornou em 1821 a província brasileira da Cisplatina. Ele ficou envolvido durante boa parte da década de 1820 no esforço brasileiro para manter a Cisplatina como parte de seu território, primeiro durante a independência do Brasil e depois na Guerra da Cisplatina. No final a província conseguiu se separar e se tornou a nação independente do Uruguai.';
+
+text.match(/m/); //["m"]
+
+text.match(/m/g); //(13) ["m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m"]
+
+text.match(/de/g); //(18) ["de", "de", "de", "de", "de", "de", "de", "de", "de", "de", "de", "de", "de", "de", "de", "de", "de", "de"]
