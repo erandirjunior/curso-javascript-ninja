7e41b1744cbc7f2bd85318f55f7b2229f9f06362
Erandir Junior <aefs12junior@gmail.com>
2018-05-22 21:15:34 -0300

    challenge-27

---
+++ challenge-27/challenge-27.js
@@ -0,0 +1,116 @@
+(function() {
+
+	'use strict';
+
+	/*
+	Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
+	métodos semelhantes aos que existem no array, mas que sirvam para os
+	elementos do DOM selecionados.
+	Crie os seguintes métodos:
+	- forEach, map, filter, reduce, reduceRight, every e some.
+
+	Crie também métodos que verificam o tipo do objeto passado por parâmetro.
+	Esses métodos não precisam depender de criar um novo elmento do DOM, podem
+	ser métodos estáticos.
+
+	Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
+	no objeto, como nos exemplos abaixo:
+	DOM.isArray([1, 2, 3]); // true
+	DOM.isFunction(function() {}); // true
+	DOM.isNumber('numero'); // false
+
+	Crie os seguintes métodos para verificação de tipo:
+	- isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+	O método isNull deve retornar `true` se o valor for null ou undefined.
+	*/
+
+	function DOM(elements) {
+		this.element = document.querySelectorAll(elements);
+	}
+
+	DOM.prototype.on = function on(eventType, callback) {
+		Array.prototype.forEach.call(this.element, function(element) {
+			element.addEventListener(eventType, callback);
+		});
+	};
+
+	DOM.prototype.off = function off(eventType, callback) {
+		Array.prototype.forEach.call(this.element, function(element) {
+			element.removeEventListener(eventType, callback);
+		});
+	};
+
+	DOM.prototype.get = function get() {
+		return this.element;
+	};
+
+	DOM.prototype.forEach = function forEach(callback) {
+		Array.prototype.forEach.call(this.element, callback);
+	}
+
+	DOM.prototype.map = function map(callback) {
+		return Array.prototype.map.call(this.element, callback);
+	}
+
+	DOM.prototype.filter = function filter(callback) {
+		return Array.prototype.filter.call(this.element, callback);
+	}
+
+	DOM.prototype.reduce = function reduce(callback) {
+		return Array.prototype.reduce.call(this.element, callback);
+	}
+
+	DOM.prototype.reduceRight = function reduceRight(callback) {
+		return Array.prototype.reduceRight.call(this.element, callback);
+	}
+
+	DOM.prototype.every = function every(callback) {
+		return Array.prototype.every.call(this.element, callback);
+	}
+
+	DOM.prototype.some = function some(callback) {
+		return Array.prototype.some.call(this.element, callback);
+	}
+
+	DOM.prototype.is = function is(arg) {
+		return Object.prototype.toString.call(arg);
+	}
+
+	DOM.prototype.isArray = function isArray(arg) {
+		return DOM.prototype.is(arg) === '[object Array]';
+	}
+
+	DOM.prototype.isObject = function isObject(arg) {
+		return DOM.prototype.is(arg) === '[object Object]';
+	}
+
+	DOM.prototype.isFunction = function isFunction(arg) {
+		return DOM.prototype.is(arg) === '[object Function]';
+	}
+
+	DOM.prototype.isNumber = function isNumber(arg) {
+		return DOM.prototype.is(arg) === '[object Number]';
+	}
+
+	DOM.prototype.isString = function isString(arg) {
+		return DOM.prototype.is(arg) === '[object String]';
+	}
+
+	DOM.prototype.isBoolean = function isBoolean(arg) {
+		return DOM.prototype.is(arg) === '[object Boolean]';
+	}
+
+	DOM.prototype.isNull = function isNull(arg) {
+		return DOM.prototype.is(arg) === '[object Null]' || DOM.prototype.is(arg) === '[object Undefined]';
+	}
+
+	var $a = new DOM('[data-js="link"]');
+	$a.on('click', function(e) {
+	  e.preventDefault();
+	  console.log('clicou');
+	});
+	
+	console.log('Elementos selecionados:', $a.get());
+	console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+})();
