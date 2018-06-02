61315a88582b29531cc75c34d6888b0a9cd18077
Erandir Junior <aefs12junior@gmail.com>
2018-06-01 21:16:23 -0300

    challenge-29

---
+++ challenge-29/challenge-29.js
@@ -0,0 +1,120 @@
+(function(DOM) {
+
+  'use strict';
+
+  /*
+  Vamos estruturar um pequeno app utilizando módulos.
+  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
+  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
+  seguinte forma:
+  - No início do arquivo, deverá ter as informações da sua empresa - nome e
+  telefone (já vamos ver como isso vai ser feito)
+  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
+  um formulário para cadastro do carro, com os seguintes campos:
+    - Imagem do carro (deverá aceitar uma URL)
+    - Marca / Modelo
+    - Ano
+    - Placa
+    - Cor
+    - e um botão "Cadastrar"
+
+  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
+  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
+  aparecer no final da tabela.
+
+  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
+  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
+  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
+  no arquivo company.json que já está criado.
+
+  Essas informações devem ser adicionadas no HTML via Ajax.
+
+  Parte técnica:
+  Separe o nosso módulo de DOM criado nas últimas aulas em
+  um arquivo DOM.js.
+
+  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
+  que será nomeado de "app".
+  */
+
+  function app() {
+    var $informationSite = new DOM('[data-js="site-information"]');
+    var $form = new DOM('[data-js="form"]');
+    var $tableBody = new DOM('[data-js="body-table"]');
+    var fragment = document.createDocumentFragment('tr');
+    var ajax = new XMLHttpRequest();
+    $informationSite.on('load', handleInformationSite);
+    $form.on('submit', handleFormCar);
+
+    function handleFormCar(event) {
+      event.preventDefault();
+      $tableBody.get()[0].appendChild(createDataTableFragment());
+    }
+
+    function handleInformationSite() {
+      ajax.open('GET', 'http://localhost:8080/company.json');
+      ajax.send();
+      ajax.addEventListener('readystatechange', handleReadyStateChange);
+    }
+
+    function handleReadyStateChange() {
+      if (isRequestOk()) {
+        var data = loadData();
+        $informationSite.get()[0].textContent = data.name;
+        $informationSite.get()[1].textContent = data.phone;      
+      }
+    }
+
+    function createDataTableFragment() {
+      fragment.appendChild(handleTrTdTable());
+      return fragment;
+    }
+
+    function handleTrTdTable() {
+      var tr = createElement('tr');
+      var tdImagem = createElement('td');
+      var tdMarca = createElement('td');
+      var tdAno = createElement('td');
+      var tdPlaca = createElement('td');
+      var tdCor = createElement('td');
+      var image = createElement('img');
+
+      image.setAttribute('src', $form.get()[0][0].value);
+      tdImagem.appendChild(image);
+      tdMarca.textContent = $form.get()[0][1].value;
+      tdAno.textContent = clearAge();
+      tdPlaca.textContent = $form.get()[0][3].value;
+      tdCor.textContent = $form.get()[0][4].value;
+
+      tr.appendChild(tdImagem);
+      tr.appendChild(tdMarca);
+      tr.appendChild(tdAno);
+      tr.appendChild(tdPlaca);
+      tr.appendChild(tdCor);
+
+      return tr;
+    }
+
+    function clearAge() {
+      console.log($form.get()[0][2].value.replace(/\D/, ''));
+      return $form.get()[0][2].value.replace(/\D/g, '');
+    }
+
+    function createElement(element) {
+      return document.createElement(element);
+    }
+
+    function loadData() {
+      return JSON.parse(ajax.responseText);
+    }
+
+    function isRequestOk() {
+      return ajax.readyState == 4 && ajax.status == 200;
+    }
+
+    return handleInformationSite;
+  }
+
+  window.addEventListener('load', app()());
+
+})(window.DOM);
