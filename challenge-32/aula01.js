8a0ad8d75235439531eb71a25663b797022825bd
Erandir Junior <aefs12junior@gmail.com>
2018-06-03 19:10:21 -0300

    Rest - cors

--- challenge-32/aula01.js
+++ challenge-32/aula01.js
@@ -5,12 +5,12 @@
 	// REST Api
 
 	var ajax = new XMLHttpRequest();
-	ajax.open('GET', 'http://localhost:3000/user');
+	ajax.open('GET', 'http://localhost:3000/user/junior');
 	ajax.send();
 
 	ajax.addEventListener('readystatechange', function(e) {
-		if (ajax.readyState === 4 && ajax.status === 200) {
-			console.log(ajax.responseText);
+		if (ajax.readyState === 4) {
+			console.log(ajax.responseText, ajax.status);
 		}
 	}, false);
 
--- challenge-32/rest-api/index.js
+++ challenge-32/rest-api/index.js
@@ -1,8 +1,25 @@
 'use strict';
 
+// Importando libs
 var express = require('express');
+var cors = require('cors');
 var app = express();
 
+var users = {
+	joao: {
+		nome: 'João',
+		idade: 30
+	},
+	maria: {
+		nome: 'Maria',
+		idade: 23
+	}
+};
+
+// Autorizando o cors
+app.use(cors());
+
+// Rotas
 app.get('/', function(req, res) {
 	res.send('<h1>Home</h1>');
 });
@@ -11,8 +28,15 @@ app.get('/user', function(req, res) {
 	res.send('<h1>User</h1>');
 });
 
-app.get('/arroz', function(req, res) {
-	res.send('arroz');
+// Rota dinâmica
+app.get('/user/:username', function(req, res) {
+	// Retornando dados
+	var username = req.params.username;
+	if (users[username]) {
+		return res.json(users[username]);
+	}
+	res.status(404).json({ error: 'Usuário não encontrado' });
 });
 
+// Seleciona a porta utilizada
 app.listen(3000);
